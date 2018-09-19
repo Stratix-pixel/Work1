2f3feb09e18b182f64caa955bf7c7ff9af4c76b8
Alexandru Achirei <alex_vaslui@yahoo.com>
2018-09-19 15:11:25 +0300

    prob6
    version 1.1

---
+++ prob6.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class prob6 {
+
+    public static void main(String[] args) {
+        //lab1, prob6
+
+        int i,f;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the centimeters of the person: ");
+
+        double centi = sc.nextDouble();
+
+        //double feet = (0.03280839895013123359580052493438 * centi);
+        double inch = Math.round((centi % 30.48)/2.54);
+        double feet = centi/30.48;
+
+        i = (int)inch;
+        f = (int)feet;
+
+        System.out.printf("The imperial height of the person is " + f +" feet " + i + " inches");
+
+
+    }
+
+}
