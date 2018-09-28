34a6d476480cfdbaede5a4a019f055230c1a0877
Alexandru Achirei <alex_vaslui@yahoo.com>
2018-09-28 22:40:26 +0300

    Update lab2prob6.java

--- Week2/lab2prob6.java
+++ Week2/lab2prob6.java
@@ -3,7 +3,7 @@
 public class lab2prob6 {
     public static void main(String[] args) {
 
-        int[] array = {1, 4, 4, 5, 7, 8, 7, 2, 2, 9, 9};
+        int[] array = {1, 4, 4, 4, 5, 7, 8, 7, 2, 2, 2, 2, 9, 9, 10};
         String arrays = consecutiveArray(array);
         System.out.println(arrays);
 
@@ -15,6 +15,9 @@ public class lab2prob6 {
 
         for (int i = 0; i < array.length - 1; i++){
             if ( array[i] == array[i+1]){
+                while (array[i] == array[i+1]) {
+                    i++;
+                }
                 result += array[i] + ", ";
                 element=result.length()-2;
             }
