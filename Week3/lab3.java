2f610584432a5be4817f3a21975d7609cba00184
Alexandru Achirei <alex_vaslui@yahoo.com>
2018-10-04 08:15:22 +0300

    Update lab3.java

--- Week3/lab3.java
+++ Week3/lab3.java
@@ -6,7 +6,7 @@ public class lab3 {
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 4};
         //a = increaseSize(a,1);
-        a = insertAt(a, 1, 7);
+        a = insertAt(a, 1, 9);
         //a = deleteAt(a, 2);
         print(a);
     }
@@ -21,34 +21,34 @@ public class lab3 {
     }
 
     public static int[] insertAt(int[] x, int index, int value) {
-        x = increaseSize(x, x.length+1);
-        int[] y = new int[x.length];
-        for (int j = 0; j < y.length-1; j++) {
-            y[j] = x[j];
+        if (index < 0 || index > x.length) {
+            System.out.println("index " + index + " is out of range");
+            return x;
         }
-        for (int i = y.length-1; i >= index; i--) {
-            y[i] = y[i-1];
+        int[] newArray = new int[x.length + 1];
+        for (int i = 0; i < newArray.length; i++) {
+            if (i == index)
+                newArray[i] = value;
+            if (i < index)
+                newArray[i] = x[i];
+            if (i > index)
+                newArray[i] = x[i - 1];
         }
-        y[index]= value;
-        return y;
+        return newArray;
     }
 
-
-    public static int[] deleteAt(int[] x, int index) {
-        int[] z =new int[x.length-1];
-        for (int i = index; i < x.length-1; i++)
-        {
-                x[i] = x[i+1];
+        public static int[] deleteAt ( int[] x, int index){
+            int[] z = new int[x.length - 1];
+            for (int i = index; i < x.length - 1; i++) {
+                x[i] = x[i + 1];
+            }
+            for (int j = 0; j < z.length; j++) {
+                z[j] = x[j];
+            }
+            return z;
         }
-        for(int j = 0; j < z.length; j++){
-            z[j] = x[j];
+
+        public static void print ( int[] x){
+            System.out.println(Arrays.toString(x));
         }
-        return z;
-    }
-
-
-    public static void print(int[] x) {
-        System.out.println(Arrays.toString(x));
-    }
-
 }
