aa175eff26a8ea3b46c33bc66f060cf95bf9f488
Alexandru Achirei <alex_vaslui@yahoo.com>
2018-10-25 13:09:32 +0300

    Update Mp3.java

--- Week5/ManageDigitalLibrary/Mp3.java
+++ Week5/ManageDigitalLibrary/Mp3.java
@@ -1,10 +1,13 @@
 package ManageDigitalLibrary;
 
-public class Mp3{
-      private String singer;
-      private String album;
+import java.util.Objects;
 
-    public Mp3(String singer, String album) {
+public class Mp3 extends MediaEntity{
+    private String singer;
+    private String album;
+
+    public Mp3(String type, String title, Integer noOfDownloads, String singer, String album) {
+        super(type, title, noOfDownloads);
         this.singer = singer;
         this.album = album;
     }
@@ -25,6 +28,29 @@ public class Mp3{
         this.album = album;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Mp3)) return false;
+        if (!super.equals(o)) return false;
+        Mp3 mp3 = (Mp3) o;
+        return Objects.equals(singer, mp3.singer) &&
+                Objects.equals(album, mp3.album);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), singer, album);
+    }
 
+    @Override
+    public String toString() {
+        return "Mp3{" + "type=" + this.getType() +
+                ", title=" + this.getTitle() +
+                ", noOfDownloads="+ this.getNoOfDownloads()+
+                ", singer='" + singer + '\'' +
+                ", album='" + album + '\'' +
+                '}';
+    }
 }
+
