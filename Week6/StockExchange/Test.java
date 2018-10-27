d8ce19c0f4e169a65a15318420d15c904780b39d
Alexandru Achirei <alex_vaslui@yahoo.com>
2018-10-27 08:59:49 +0300

    Update Test.java

--- Week6/StockExchange/Test.java
+++ Week6/StockExchange/Test.java
@@ -1,4 +1,4 @@
-
+package StockExchange;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,11 +8,12 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
 
-        Date myDate = new Date();
-        Date myDate1 = new Date(2018,8,13);
+        Date myDate = new Date(2018, 10, 15);
 
-        StockUpdate su1 = new StockUpdate("AMZN", myDate,55.44);
-        StockUpdate su2= new StockUpdate("MSFT", myDate1,36.56);
+        StockUpdate su1 = new StockUpdate("AMZN", new Date(2018, 8, 15),55.44);
+        StockUpdate su2 = new StockUpdate("MSFT", new Date(2018, 9, 15),36.56);
+        StockUpdate su3 = new StockUpdate("INTC", new Date(2018, 6, 11),63.29);
+        StockUpdate su4 = new StockUpdate("TSLA", new Date(2018, 10, 23),44.19);
 
 
         List<StockUpdate> listOfUpdates = new LinkedList<>();
@@ -22,8 +23,12 @@ public class Test {
 
         sm1.add(su1);
         sm1.add(su2);
+        sm1.add(su3);
+        sm1.add(su4);
 
         System.out.println(sm1);
+
+
 
 
     }
