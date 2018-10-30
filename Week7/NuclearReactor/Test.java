5c7e2ddae0841cb547781247ec05f5a49b3a1804
Alexandru Achirei <alex_vaslui@yahoo.com>
2018-10-30 22:32:37 +0200

    Update Test.java

--- Week7/NuclearReactor/Test.java
+++ Week7/NuclearReactor/Test.java
@@ -1,12 +1,12 @@
 package NuclearReactor;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         PowerPlant pp1 = new PowerPlant(120);
         Reactor r1 = new Reactor(150);
         PlantController pc1 = new PlantController(pp1, r1);
 
-        pc1.runSystem(r1, pp1);
+        pc1.runSystem();
     }
 
 
