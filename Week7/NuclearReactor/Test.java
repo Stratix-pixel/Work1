b2a5e2851596d4210c69df7889521296929abecc
Alexandru Achirei <alex_vaslui@yahoo.com>
2018-10-30 19:47:10 +0200

    Add files via upload

---
+++ Week7/NuclearReactor/Test.java
@@ -0,0 +1,10 @@
+package NuclearReactor;
+
+public class Test {
+    public static void main(String[] args) {
+        PowerPlant pp1 = new PowerPlant(120);
+        Reactor r1 = new Reactor(150);
+
+
+    }
+}
