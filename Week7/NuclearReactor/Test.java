4039814c0267448f083c8b3a2b64070585b389f1
Alexandru Achirei <alex_vaslui@yahoo.com>
2018-10-30 20:02:49 +0200

    Update Test.java

--- Week7/NuclearReactor/Test.java
+++ Week7/NuclearReactor/Test.java
@@ -4,7 +4,10 @@ public class Test {
     public static void main(String[] args) {
         PowerPlant pp1 = new PowerPlant(120);
         Reactor r1 = new Reactor(150);
+        PlantController pc1 = new PlantController(pp1, r1);
 
-
+        pc1.runSystem(r1, pp1);
     }
+
+
 }
